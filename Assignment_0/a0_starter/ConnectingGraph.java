5050af2cab43a9175816ca009d94a15c72e440a3
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-24 11:10:22 -0400

    asdasdad

--- Assignment_0/a0_starter/ConnectingGraph.java
+++ Assignment_0/a0_starter/ConnectingGraph.java
@@ -10,27 +10,21 @@ class ConnectingGraph {
     }
 
     public int find(int x) {
-        int j, fx;
-        j = x;
+        if (father.get(x) == null) {
+            father.put(x, x);
+            return x;
+        }
 
-        if (father.containsKey(j) == false) {
-            father.put(j, j);
-            return j;
+        if (father.get(x) == x) {
+            return x;
         }
-        
-        // find x 的 big brother
-        while (father.get(j) != j) {
-            j = father.get(j);
-        }
-        
+
         // path compression
-        while (x != j) {
-            fx = father.get(x);
-            father.put(x, j);
-            x = fx;
-        }
-        
-        return j;
+        int root_father = find(father.get(x));
+
+        father.put(x, root_father);
+
+        return root_father;
     }
 
     public void connect(int a, int b) {
@@ -51,10 +45,10 @@ class ConnectingGraph {
     }
 
     public HashMap<Integer, Integer> getFatherRelation() {
-        for (Map.Entry<Integer, Integer> entry : father.entrySet()) {
-            int new_value = find(entry.getKey());
-            father.put(entry.getKey(), new_value);
-        }
+        // for (Map.Entry<Integer, Integer> entry : father.entrySet()) {
+        //     int new_value = find(entry.getKey());
+        //     father.put(entry.getKey(), new_value);
+        // }
         
         return father;
     }
