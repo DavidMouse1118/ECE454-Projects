3eb3be2217c9a9a79598cc360c3b12da394c5649
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-24 19:20:36 -0400

    adad

--- Assignment_0/a0_starter/ConnectingGraph.java
+++ Assignment_0/a0_starter/ConnectingGraph.java
@@ -12,41 +12,43 @@ class ConnectingGraph {
     public int find(int x) {
         int j, fx;
         j = x;
-
-        if (father.containsKey(j) == false) {
-            father.put(j, j);
-            return j;
-        }
         
         // find x's root father
         while (father.get(j) != j) {
             j = father.get(j);
         }
-        
+
         // // path compression
         // while (x != j) {
-        //     fx = father.get(x);
-        //     father.put(x, j);
-        //     x = fx;
+        // fx = father.get(x);
+        // father.put(x, j);
+        // x = fx;
         // }
-        
+
         return j;
     }
 
     public void union(int a, int b) {
+        if (!father.containsKey(a)) {
+            father.put(a, a);
+        }
+
+        if (!father.containsKey(b)) {
+            father.put(b, b);
+        }
         int A = find(a);
         int B = find(b);
-        
+
         if (A != B) {
             // int heightA = height.getOrDefault(A, 0);
             // int heightB = height.getOrDefault(B, 0);
             // if (heightA > heightB) {
-            //     father.put(B, A);
+            // father.put(B, A);
             // } else if (heightA < heightB) {
-            //     father.put(A, B);
+            // father.put(A, B);
             // } else {
-                father.put(A, B);
-            //     height.put(B, heightB + 1);
+            father.put(A, B);
+            // height.put(B, heightB + 1);
             // }
         }
     }
@@ -57,7 +59,7 @@ class ConnectingGraph {
             int new_value = find(entry.getKey());
             father.put(entry.getKey(), new_value);
         }
-        
+
         return father;
     }
 }
