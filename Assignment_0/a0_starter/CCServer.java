11291cacc4de6989f5dc24c59ff1ec891e874537
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-23 21:45:02 -0400

    format document

--- Assignment_0/a0_starter/CCServer.java
+++ Assignment_0/a0_starter/CCServer.java
@@ -2,45 +2,41 @@ import java.io.*;
 import java.net.*;
 
 class CCServer {
-    public static void main(String args[]) throws Exception {
-	if (args.length != 1) {
-	    System.out.println("usage: java CCServer port");
-	    System.exit(-1);
-	}
-	int port = Integer.parseInt(args[0]);
+	public static void main(String args[]) throws Exception {
+		if (args.length != 1) {
+			System.out.println("usage: java CCServer port");
+			System.exit(-1);
+		}
+		int port = Integer.parseInt(args[0]);
 
-	ServerSocket ssock = new ServerSocket(port);
-	System.out.println("listening on port " + port);
-	while(true) {
-	    try {
-			Socket csock = ssock.accept();
-			System.out.println("Accepted connections: " + csock);
+		ServerSocket ssock = new ServerSocket(port);
+		System.out.println("listening on port " + port);
+		while (true) {
+			try {
+				Socket csock = ssock.accept();
+				System.out.println("Accepted connections: " + csock);
 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(csock.getInputStream()));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(csock.getInputStream()));
 
-			PrintWriter writer = new PrintWriter(csock.getOutputStream(), true);
+				PrintWriter writer = new PrintWriter(csock.getOutputStream(), true);
 
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line);
+				}
+
+				writer.println(line.toUpperCase());
+				csock.close();
+				/*
+				 * YOUR CODE GOES HERE - accept connection from server socket - read requests
+				 * from connection repeatedly - for each request, compute an output and send a
+				 * response - each message has a 4-byte header followed by a payload - the
+				 * header is the length of the payload (signed, two's complement, big-endian) -
+				 * the payload is a string (UTF-8, big-endian)
+				 */
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-			writer.println(line.toUpperCase());
-			csock.close();
-		/*
-		  YOUR CODE GOES HERE
-		  - accept connection from server socket
-		  - read requests from connection repeatedly
-		  - for each request, compute an output and send a response
-		  - each message has a 4-byte header followed by a payload
-		  - the header is the length of the payload
-		    (signed, two's complement, big-endian)
-		  - the payload is a string
-		    (UTF-8, big-endian)
-		*/
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
+		}
 	}
-    }
 }
