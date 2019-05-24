6bf83f249e729efec10c5bc90c9ea323db2c043b
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-24 19:22:36 -0400

    adad

--- Assignment_0/a0_starter/ConnectingGraph.java
+++ Assignment_0/a0_starter/ConnectingGraph.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class ConnectingGraph {
     private HashMap<Integer, Integer> father = null;
-    // private HashMap<Integer, Integer> height = null;
+    private HashMap<Integer, Integer> height = null;
 
     public ConnectingGraph() {
         father = new HashMap<Integer, Integer>();
-        // height = new HashMap<Integer, Integer>();
+        height = new HashMap<Integer, Integer>();
     }
 
     public int find(int x) {
         int j, fx;
         j = x;
-        
+
         // find x's root father
         while (father.get(j) != j) {
             j = father.get(j);
@@ -31,25 +31,28 @@ class ConnectingGraph {
     public void union(int a, int b) {
         if (!father.containsKey(a)) {
             father.put(a, a);
+            height.put(a, 0);
         }
 
         if (!father.containsKey(b)) {
             father.put(b, b);
+            height.put(b, 0);
         }
+
         int A = find(a);
         int B = find(b);
 
         if (A != B) {
-            // int heightA = height.getOrDefault(A, 0);
-            // int heightB = height.getOrDefault(B, 0);
-            // if (heightA > heightB) {
-            // father.put(B, A);
-            // } else if (heightA < heightB) {
-            // father.put(A, B);
-            // } else {
-            father.put(A, B);
-            // height.put(B, heightB + 1);
-            // }
+            int heightA = height.get(A);
+            int heightB = height.get(B);
+            if (heightA > heightB) {
+                father.put(B, A);
+            } else if (heightA < heightB) {
+                father.put(A, B);
+            } else {
+                father.put(A, B);
+                height.put(B, heightB + 1);
+            }
         }
     }
 
