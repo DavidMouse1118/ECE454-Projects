a8f11aaed51d6cb0afde35812775dde7230811d6
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-25 12:59:27 -0400

    wt

--- Assignment_0/a0_starter/ConnectingGraph.java
+++ Assignment_0/a0_starter/ConnectingGraph.java
@@ -29,15 +29,15 @@ class ConnectingGraph {
     }
 
     public void union(int a, int b) {
-        // if (!father.containsKey(a)) {
-        //     father.put(a, a);
-        //     height.put(a, 0);
-        // }
+        if (!father.containsKey(a)) {
+            father.put(a, a);
+            height.put(a, 0);
+        }
 
-        // if (!father.containsKey(b)) {
-        //     father.put(b, b);
-        //     height.put(b, 0);
-        // }
+        if (!father.containsKey(b)) {
+            father.put(b, b);
+            height.put(b, 0);
+        }
 
         int A = find(a);
         int B = find(b);
