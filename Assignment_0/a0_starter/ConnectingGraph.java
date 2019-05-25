633f2389d6bfcd2f23fcc816ccfaa75db7eff289
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-05-25 12:50:12 -0400

    wt

--- Assignment_0/a0_starter/ConnectingGraph.java
+++ Assignment_0/a0_starter/ConnectingGraph.java
@@ -29,32 +29,32 @@ class ConnectingGraph {
     }
 
     public void union(int a, int b) {
-        if (!father.containsKey(a)) {
-            father.put(a, a);
-            height.put(a, 0);
-        }
+        // if (!father.containsKey(a)) {
+        //     father.put(a, a);
+        //     height.put(a, 0);
+        // }
 
-        if (!father.containsKey(b)) {
-            father.put(b, b);
-            height.put(b, 0);
-        }
+        // if (!father.containsKey(b)) {
+        //     father.put(b, b);
+        //     height.put(b, 0);
+        // }
 
-        int A = find(a);
-        int B = find(b);
+        // int A = find(a);
+        // int B = find(b);
 
-        if (A != B) {
-            int heightA = height.get(A);
-            int heightB = height.get(B);
+        // if (A != B) {
+        //     int heightA = height.get(A);
+        //     int heightB = height.get(B);
 
-            if (heightA > heightB) {
-                father.put(B, A);
-            } else if (heightA < heightB) {
-                father.put(A, B);
-            } else {
-                father.put(A, B);
-                height.put(B, heightB + 1);
-            }
-        }
+        //     if (heightA > heightB) {
+        //         father.put(B, A);
+        //     } else if (heightA < heightB) {
+        //         father.put(A, B);
+        //     } else {
+        //         father.put(A, B);
+        //         height.put(B, heightB + 1);
+        //     }
+        // }
     }
 
     public HashMap<Integer, Integer> refreshFatherRelation() {
