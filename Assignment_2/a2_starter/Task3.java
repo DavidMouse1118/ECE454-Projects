25f1efef80ddf645b137b6baf390fe6671d0d391
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-06-27 14:05:42 -0400

    finish task3 hadoop

--- Assignment_2/a2_starter/Task3.java
+++ Assignment_2/a2_starter/Task3.java
@@ -14,9 +14,37 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Task3 {
 
-  // add code here
+  public static class UserRatingMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private IntWritable userId = new IntWritable();
 
-    
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] tokens = value.toString().split(",");
+      for (int i = 1; i < tokens.length; i++) {
+        if (tokens[i].equals("")) {
+          continue;
+        }
+
+        userId.set(i);
+        context.write(userId, one);
+      }
+    }
+  }
+
+  public static class UserRatingCountReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+
+      int count = 0;
+      for (IntWritable val : values) {
+        count += val.get();
+      }
+      result.set(count);
+      context.write(key, result);
+    }
+  }
     
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
@@ -27,7 +55,12 @@ public class Task3 {
     Job job = new Job(conf, "Task3");
     job.setJarByClass(Task3.class);
 
-    // add code here
+    job.setMapperClass(UserRatingMapper.class);
+    job.setCombinerClass(UserRatingCountReducer.class);
+    job.setReducerClass(UserRatingCountReducer.class);
+
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(IntWritable.class);
 
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
---
+++ Assignment_2/a2_starter/buildrun_Task3_hadoop_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export CLASSPATH=`hadoop classpath`
+
+echo --- Deleting
+rm Task3.jar
+rm Task3*.class
+
+echo --- Compiling
+$JAVA_HOME/bin/javac Task3.java
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+echo --- Jarring
+$JAVA_HOME/bin/jar -cf Task3.jar Task3*.class
+
+echo --- Running
+INPUT=/tmp/in3.txt
+OUTPUT=/user/${USER}/a2_starter_code_output/
+
+hdfs dfs -rm -R $OUTPUT
+hdfs dfs -copyFromLocal sample_input/smalldata.txt /tmp
+time yarn jar Task3.jar Task3 $INPUT $OUTPUT
+
+hdfs dfs -ls $OUTPUT
