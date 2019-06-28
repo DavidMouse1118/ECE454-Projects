74c1f240b77999c763fe5343d2cd52e80232d0c6
DavidMouse1118 <zhidongzhang2015@gmail.com>
2019-06-27 20:03:34 -0400

    finish task4 spark

--- Assignment_2/a2_starter/Task4.scala
+++ Assignment_2/a2_starter/Task4.scala
@@ -1,7 +1,25 @@
 import org.apache.spark.{SparkContext, SparkConf}
+import scala.math.min
 
 // please don't change the object name
+// Performance: 44.21user 5.67system 7:04.37elapsed 11%CPU (0avgtext+0avgdata 755708maxresident)k
 object Task4 {
+  def findSimilarity(movie1: String, movie2: String): String = {
+    val ratings1 = movie1.split(",")
+    val ratings2 = movie2.split(",")
+
+    val moviePair = ratings1(0) + "," + ratings2(0)
+    var similarity = 0
+
+    for(i <- 1 until min(ratings1.length, ratings2.length)){
+      if (ratings1(i) == ratings2(i) && ratings1(i) != "") {
+        similarity += 1
+      }
+    }
+
+    return moviePair + "," + similarity
+  } 
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 4")
     val sc = new SparkContext(conf)
@@ -9,8 +27,9 @@ object Task4 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
-    output.saveAsTextFile(args(1))
+    val output = textFile.cartesian(textFile)
+      .filter{case (a,b) => a < b}
+      .map{case (a, b) => findSimilarity(a, b)}
+      .saveAsTextFile(args(1))
   }
 }
---
+++ Assignment_2/a2_starter/buildrun_Task4_spark_cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export SCALA_HOME=/usr
+export CLASSPATH=".:/opt/spark-latest/jars/*"
+
+echo --- Deleting
+rm Task4.jar
+rm Task4*.class
+
+echo --- Compiling
+$SCALA_HOME/bin/scalac -J-Xmx1g Task4.scala
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+echo --- Jarring
+$JAVA_HOME/bin/jar -cf Task4.jar Task4*.class
+
+echo --- Running
+INPUT=/tmp/in3.txt
+OUTPUT=/user/${USER}/a2_starter_code_output/
+
+hdfs dfs -rm -R $OUTPUT
+hdfs dfs -copyFromLocal sample_input/smalldata_z498zhan.txt /tmp
+time spark-submit --master yarn --class Task4 Task4.jar $INPUT $OUTPUT
+
+hdfs dfs -ls $OUTPUT
